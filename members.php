bf7069cf90a48392e867ca6fc5c66fb8ed457909
nicolas-camilloni <nicolas.camilloni@laplateforme.io>
2020-01-08 14:03:01 +0100

    members threads topics config functions img

---
+++ members.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Liste des membres</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+    <main>
+        <section class="cmid">
+        <section class="listmembre">
+        <h1 id="ctitlemembre">Liste des membres</h1>
+<?php
+
+$connexion = mysqli_connect("localhost", "root","", "forum");
+$requete = "SELECT * FROM utilisateurs";
+$query = mysqli_query($connexion, $requete);
+$resultat = mysqli_fetch_all($query);
+$i = 0;
+
+if( isset($_SESSION['login']) == true )
+{
+    foreach($resultat as $cle => $valeur) {
+        $i += 1;
+        if ( $i%2 == 1) {
+            ?>
+            <a href="user.php?userid=<?php echo $resultat[$cle][0]; ?>"><section class="cmembre mimpair">
+                <img alt="photoprofil" class="photoprofil" src="<?php echo $resultat[$cle][7] ?>">
+                <article class="pseudograde">
+                    <p class="pseudo"><?php echo $resultat[$cle][1]; ?></p>
+                    <?php
+                        if ( $resultat[$cle][5] == 1 ) {
+                        ?>
+                        <p class="grade">Administrateur</p>
+                        <?php
+                        }
+                        if ( $resultat[$cle][5] == 2 ) {
+                        ?>
+                        <p class="grade">Modérateur</p>
+                        <?php
+                        }
+                        if ( $resultat[$cle][5] == 3 ) {
+                        ?>
+                        <p class="grade">Membre</p>
+                        <?php
+                        }
+                    ?>
+                </article>
+                <article class="inscritle">
+                    <p>Inscrit depuis le <?php echo date('d-m-Y à H:i:s', strtotime($resultat[$cle][6])); ?></p>
+                </article>
+            </section></a>
+            <?php
+        }
+        else {
+            ?>
+            <a href="user.php?userid=<?php echo $resultat[$cle][0]; ?>"><section class="cmembre mpair">
+                <img alt="photoprofil" class="photoprofil" src="<?php echo $resultat[$cle][7] ?>">
+                <article class="pseudograde">
+                    <p class="pseudo"><?php echo $resultat[$cle][1]; ?></p>
+                    <?php
+                        if ( $resultat[$cle][5] == 1 ) {
+                        ?>
+                        <p class="grade">Administrateur</p>
+                        <?php
+                        }
+                        if ( $resultat[$cle][5] == 2 ) {
+                        ?>
+                        <p class="grade">Modérateur</p>
+                        <?php
+                        }
+                        if ( $resultat[$cle][5] == 3 ) {
+                        ?>
+                        <p class="grade">Membre</p>
+                        <?php
+                        }
+                    ?>
+                </article>
+                <article class="inscritle">
+                    <p>Inscrit depuis le <?php echo date('d-m-Y à H:i:s', strtotime($resultat[$cle][6])); ?></p>
+                </article>
+            </section>
+            </a>
+            <?php
+        }
+    }
+}
+else
+{
+    echo "Vous n'avez pas accès à cette page";
+}
+?>
+        </section>
+        </section>
+    </main>
+    <?php include("footer.php"); ?>
+</body>
+
+</html>
+<?php
+mysqli_close($connexion);
---
+++ threads.php
@@ -0,0 +1,59 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Topics</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+<?php include("header.php"); ?>
+    <main>
+        <section class="cmid">
+            <article>
+            <?php
+            date_default_timezone_set('Europe/Paris');
+            $cnx = mysqli_connect("localhost", "root", "", "forum");
+            $idtopic = $_GET['idtopic'];
+            $requete = "SELECT * FROM threads WHERE id_topic=$idtopic ORDER BY datethread DESC";
+            $query = mysqli_query($cnx, $requete);
+            $resultat = mysqli_fetch_all($query, MYSQLI_ASSOC);
+            $taille = count($resultat);
+            $a = 0;
+            while ($a < $taille) {
+                $datesql = $resultat[$a]['datethread'];
+                $newdate = date('d-m-Y à H:i:s', strtotime($datesql));
+                $iduser = $resultat[$a]['id_utilisateur'];
+                $idthread = $resultat[$a]['id'];
+                $requetelogin = "SELECT login FROM utilisateurs WHERE id=$iduser";
+                $query2 = mysqli_query($cnx, $requetelogin);
+                $resultatlogin = mysqli_fetch_all($query2, MYSQLI_ASSOC);
+                
+                $requetederrep = "SELECT (SELECT COUNT(*) FROM messages WHERE id_thread=$idthread) as countrep, (SELECT datemessage FROM messages WHERE id_thread=$idthread AND messages.id = (SELECT MAX(messages.id) FROM messages WHERE id_thread=$idthread)) as  daterep, (SELECT login FROM utilisateurs, messages WHERE messages.id_utilisateur = utilisateurs.id AND messages.id = (SELECT MAX(messages.id) FROM messages WHERE id_thread=$idthread)) as logincrea";
+                $queryderrep = mysqli_query($cnx, $requetederrep);
+                $resultatderrep = mysqli_fetch_all($queryderrep);
+                 echo "<a href=\"messages.php?idthread=".$idthread."\"><article class=\"indexcasethread\"><p>".$resultat[$a]['nomthread']."</p><p>Créer le : ".$newdate." par : ".$resultatlogin[0]["login"]."</p><article class=\"messageder\"><p>Réponses: ".$resultatderrep[0][0]."</p><p>Dernière réponse par ".$resultatderrep[0][2]."</p><p>le ".$resultatderrep[0][1]."</p></article></article></a>";
+            $a++;
+            }
+            ?>
+        </article>
+        <article>
+        <?php
+            if (isset($_SESSION["login"])) 
+            {
+                  echo "<br>Bonjour, " . $_SESSION["login"] . " vous êtes connecté vous pouvez créer un <a href=\"new-thread.php?idtopic=".$idtopic."\">thread</a>.<br />";
+            } 
+            else{
+            	  echo "<br>Bonjour Guest, Veuillez vous connecté afin de pouvoir créer un topic.<br />";
+            }
+        ?>
+        </article>
+        </section>
+    </main>
+<?php include("footer.php"); ?>
+</body>
+
+</html>
