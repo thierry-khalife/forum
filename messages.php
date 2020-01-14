ada3cd68d99bd460e63570ea690888aa33981f29
nicolas-camilloni <nicolas.camilloni@laplateforme.io>
2020-01-14 11:45:56 +0100

    css discussion

--- messages.php
+++ messages.php
@@ -52,7 +52,7 @@ if ( isset($_POST['envoyer']) == true && isset($_POST['message']) && strlen($_PO
 <?php include("header.php"); ?>
     <main>
          <section class="cmid">
-            <h1 class="h1topic">Discussion</h1>
+            <h1 class="h1topic"><?php echo $resultatthread[0]['nomthread']; ?></h1>
             <?php
             $a = 0;
             if( !empty($resultat) && isset($_GET["idthread"]) ) {
@@ -62,47 +62,75 @@ if ( isset($_POST['envoyer']) == true && isset($_POST['message']) && strlen($_PO
                 $iduser = $resultat[$a]['id_utilisateur'];
                 $idcom = $resultat[$a]['id'];
                 $intidcom = intval($idcom);
-                $requetelogin = "SELECT login FROM utilisateurs WHERE id=$iduser";
+                $requetelogin = "SELECT login, avatar, rank, dateinscription FROM utilisateurs WHERE id=$iduser";
                 $querylogin = mysqli_query($cnx, $requetelogin);
                 $resultatlogin = mysqli_fetch_all($querylogin, MYSQLI_ASSOC);
+                $newdate2 = date('d-m-Y à H:i:s', strtotime($resultatlogin[0]["dateinscription"]));
+
                 include("like.php"); // SYSTEME DE LIKE/DISLIKE
                 ?>
                 <section class="cmessages">
-                    <article>
-                    <?php
-                    echo "<b>".$resultatlogin[0]["login"].",</b> le <i><u>".$newdate."</u></i><br /><br />";
-                    echo $resultat[$a]['message']."<br />";
-                    ?>
-                    </article>
-                    <article>
-                        <form method="post" action="messages.php?idthread=<?php echo $intidthread; ?>">
-                            <div id="formvote">
-                            <?php
-                            if ( isset($_SESSION['login']) && $resultat3[0]['COUNT(*)'] != "0" ) {
-                                echo "<input type=\"submit\" name=\"likebutton".$a."\" id=\"likev\" value=\"like\"><div class=\"resultatvotes\">".$resultat5[0]['COUNT(*)']."</div>";
-                                echo "<input type=\"submit\" name=\"dislikebutton".$a."\" id=\"dislike\" value=\"dislike\"><div class=\"resultatvotes\">".$resultat6[0]['COUNT(*)']."</div>";
-                            }
-                            elseif ( isset($_SESSION['login']) && $resultat4[0]['COUNT(*)'] != "0" ) {
-                                echo "<input type=\"submit\" name=\"likebutton".$a."\" id=\"like\" value=\"like\"><div class=\"resultatvotes\">".$resultat5[0]['COUNT(*)']."</div>";
-                                echo "<input type=\"submit\" name=\"dislikebutton".$a."\" id=\"dislikev\" value=\"dislike\"><div class=\"resultatvotes\">".$resultat6[0]['COUNT(*)']."</div>";
-                            }
-                            else {
-                                echo "<input type=\"submit\" name=\"likebutton".$a."\" id=\"like\" value=\"like\"><div class=\"resultatvotes\">".$resultat5[0]['COUNT(*)']."</div>";
-                                echo "<input type=\"submit\" name=\"dislikebutton".$a."\" id=\"dislike\" value=\"dislike\"><div class=\"resultatvotes\">".$resultat6[0]['COUNT(*)']."</div>";
-                            }
-                            ?>
-                            </div>
-                        </form>
-                    </article>
-                    <article>
+                    <article class="messageleft">
+                        <a href="user.php?userid=<?php echo $iduser; ?>"><img class="avatarmsg" src="<?php echo $resultatlogin[0]["avatar"]; ?>"></a>
+                        <h2><?php echo $resultatlogin[0]["login"]; ?></h2>
                         <?php
-                        if(isset($_SESSION['login']) && ($_SESSION['rank'] == 1 || $_SESSION['rank'] == 2))
-                        {
-                            echo "<form method=\"post\" action=\"messages.php?idthread=$intidthread\">
-                            <br><input type=\"submit\" class=\"submit2\"  name=\"delete".$a."\" value=\"$idcom\" />
-                            </form>";
+                        if ( $resultatlogin[0]["rank"] == "1" ) {
+                            ?>
+                            <img class="rank" src="img/Administrator.png" alt="admin">
+                            <?php
+                        }
+                        if ( $resultatlogin[0]["rank"] == "2" ) {
+                            ?>
+                            <img class="rank" src="img/Moderator.png" alt="modo">
+                            <?php
+                        }
+                        if ( $resultatlogin[0]["rank"] == "3" ) {
+                            ?>
+                            <img class="rank" src="img/Member.png" alt="membre">
+                            <?php
                         }
                         ?>
+                        <p> Inscrit depuis le <?php echo $newdate2; ?> </p>
+                    </article>
+                    <article class="message">
+                        <article>
+                        <?php
+                        echo "le <i><u>".$newdate."</u></i><br /><br />";
+                        echo $resultat[$a]['message']."<br />";
+                        ?>
+                        </article>
+                        <section>
+                            <article class="likebtn">
+                                <form method="post" action="messages.php?idthread=<?php echo $intidthread; ?>">
+                                    <div id="formvote">
+                                    <?php
+                                    if ( isset($_SESSION['login']) && $resultat3[0]['COUNT(*)'] != "0" ) {
+                                        echo "<input type=\"submit\" name=\"likebutton".$a."\" id=\"likev\" value=\"like\"><div class=\"resultatvotes\">".$resultat5[0]['COUNT(*)']."</div>";
+                                        echo "<input type=\"submit\" name=\"dislikebutton".$a."\" id=\"dislike\" value=\"dislike\"><div class=\"resultatvotes\">".$resultat6[0]['COUNT(*)']."</div>";
+                                    }
+                                    elseif ( isset($_SESSION['login']) && $resultat4[0]['COUNT(*)'] != "0" ) {
+                                        echo "<input type=\"submit\" name=\"likebutton".$a."\" id=\"like\" value=\"like\"><div class=\"resultatvotes\">".$resultat5[0]['COUNT(*)']."</div>";
+                                        echo "<input type=\"submit\" name=\"dislikebutton".$a."\" id=\"dislikev\" value=\"dislike\"><div class=\"resultatvotes\">".$resultat6[0]['COUNT(*)']."</div>";
+                                    }
+                                    else {
+                                        echo "<input type=\"submit\" name=\"likebutton".$a."\" id=\"like\" value=\"like\"><div class=\"resultatvotes\">".$resultat5[0]['COUNT(*)']."</div>";
+                                        echo "<input type=\"submit\" name=\"dislikebutton".$a."\" id=\"dislike\" value=\"dislike\"><div class=\"resultatvotes\">".$resultat6[0]['COUNT(*)']."</div>";
+                                    }
+                                    ?>
+                                    </div>
+                                </form>
+                            </article>
+                            <article class="deletebtn">
+                                <?php
+                                if(isset($_SESSION['login']) && ($_SESSION['rank'] == 1 || $_SESSION['rank'] == 2))
+                                {
+                                    echo "<form method=\"post\" action=\"messages.php?idthread=$intidthread\">
+                                    <br><input type=\"submit\" class=\"submitdel\"  name=\"delete".$a."\" value=\"$idcom\" />
+                                    </form>";
+                                }
+                                ?>
+                            </article>
+                        </section>
                     </article>
                 </section>
                 <?php // SYSTEME DELETE WHEN ADMIN
